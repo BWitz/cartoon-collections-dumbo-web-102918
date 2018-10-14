74ca4986bc59f9b415647490beb6f54fad039ab4
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-14 17:41:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
   counter = 1
   dwarves.each_with_index {|x| puts "#{counter += 1}. #{x}"}
 end
