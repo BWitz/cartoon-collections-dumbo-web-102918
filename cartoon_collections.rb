471f91e14c5378240bac68b99661e86447bbb212
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-14 17:38:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   counter = 1
-  dwarves.each_with_index {|x| puts "#{counter}. #{x}"}
-  counter += 1
+  dwarves.each_with_index {|x| puts "#{counter += 1}. #{x}" counter}
+  
 end
 
 def summon_captain_planet# code an argument here
